0b48521b38c477db6b9d91b0d9f8ca759fb12347
rhythminme <raj@rhythmin.me>
2020-02-18 15:19:11 +0300

    Start local server

--- config.js
+++ config.js
@@ -1,20 +1,20 @@
 const assert = require('assert');
 const camelCase = require('camelcase');
 
-export const getConfig = () => {
+module.exports.getConfig = () => {
   const port = process.env.PORT || 4001;
   const oidcMode = process.env['OIDC_MODE'] || 'implicit';
 
   const commonParams = { port };
   if (oidcMode.toLowerCase() === 'implicit') {
     return ['CLIENT_ID', 'APPLICATION_TYPE', 'CLIENT_REDIRECT_URI', 'CLIENT_LOGOUT_REDIRECT_URI'].reduce((acc, v) => {
-      assert(process.env[v], `${v} config missing`);
+      assert(process.env[v], `${v} environment variable is not set`);
       acc[camelCase(v)] = process.env[v];
       return acc;
     }, commonParams);
   }
   return ['CLIENT_ID', 'CLIENT_SECRET', 'CLIENT_REDIRECT_URI', 'CLIENT_LOGOUT_REDIRECT_URI'].reduce((acc, v) => {
-    assert(process.env[v], `${v} config missing`);
+    assert(process.env[v], `${v} environment variable is not set`);
     acc[camelCase(v)] = process.env[v];
     return acc;
   }, commonParams);
--- index.js
+++ index.js
@@ -3,41 +3,34 @@ const { getConfig } = require('./config');
 
 const config = getConfig();
 
-const oidcConfig = {
-  features: {
-    devInteractions: true,
-    discovery: true,
-    registration: false,
-    revocation: true,
-    sessionManagement: false
-  },
-  format: {
-    default: 'jwt',
-    AccessToken: 'jwt',
-    RefreshToken: 'jwt'
+const oidc = new Provider(`http://localhost:${config.port}`, {
+  clients: [
+    {
+      client_id: config.clientId,
+      client_secret: config.clientSecret,
+      redirect_uris: [config.clientRedirectUri],
+      post_logout_redirect_uris: [config.clientLogoutRedirectUri],
+      application_type: config.applicationType,
+      token_endpoint_auth_method: 'none',
+      response_types: ['id_token'],
+      grant_types: ['implicit']
+
+    }
+  ]
+});
+
+const { invalidate: orig } = oidc.Client.Schema.prototype;
+
+oidc.Client.Schema.prototype.invalidate = (message, code) => {
+  if (code === 'implicit-force-https' || code === 'implicit-forbid-localhost') {
+    return;
   }
+
+  orig.call(this, message);
 };
-
-const oidc = new Provider(`http://localhost:${config.port}`, oidcConfig);
-
-const clients = [
-  {
-    client_id: config.clientId,
-    client_secret: config.clientSecret,
-    redirect_uris: [config.clientRedirectUri],
-    post_logout_redirect_uris: [config.clientLogoutRedirectUri],
-    application_type: config.applicationType,
-    token_endpoint_auth_method: 'none',
-    response_types: ['id_token'],
-    grant_types: ['implicit']
-
-  }
-];
 
 let server;
 (async () => {
-  await oidc.initialize({ clients });
-
   server = oidc.listen(config.port, () => {
     console.log(
       `fake-oidc-server listening on port ${config.port}, check http://localhost:${config.port}/.well-known/openid-configuration`
